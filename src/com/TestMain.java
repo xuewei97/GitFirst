77f03a8e11837c1e7d589d1f97fa43b609536a09
xuewei97 <279838169@qq.com>
2020-09-25 11:45:22 +0800

    1

--- src/com/TestMain.java
+++ src/com/TestMain.java
@@ -4,9 +4,5 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("初始化导入....");
 		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
 	}
 }
